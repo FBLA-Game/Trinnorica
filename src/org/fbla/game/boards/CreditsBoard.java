521001394303ccf0943d386419b9847ce0a7c36c
Cameron Witcher <CameronWitcher2017@gmail.com>
2016-05-04 10:20:18 -0500

    sjit

--- src/org/fbla/game/boards/CreditsBoard.java
+++ src/org/fbla/game/boards/CreditsBoard.java
@@ -151,6 +151,9 @@ public class CreditsBoard extends Board implements ActionListener {
 		drawOutlineString("Background music: HeatleyBros", x("Background music: HeatleyBros",g), st+(si*42)-i,g);
 		drawOutlineString("Sound Effects: Bfxr.net", x("Sound Effects: Bfxr.net",g), st+(si*43)-i,g);
 		
+		drawOutlineString("Logo design: Tailorbrands.com", x("Logo design: Tailorbrands.com",g), st+(si*43)-i,g);
+		drawOutlineString("Sound Effects: Bfxr.net", x("Sound Effects: Bfxr.net",g), st+(si*43)-i,g);
+		
 		if(st+(si*46)-i <= 90){
 			drawOutlineString("A big thanks to FBLA for giving us the opportunity to create this game!", x("A big thanks to FBLA for giving us the opportunity to create this game!",g), 90-(si),g);
 			g.drawImage(Texture.loadTexture("fbla-logo.png"), M_WIDTH/2 - 300, 90, 600, 465, this);
